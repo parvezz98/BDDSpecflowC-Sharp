06fb8179188c1c82f36a105ade2826b9943467bd
parvezz98 <42081995+parvezz98@users.noreply.github.com>
2022-07-03 19:14:04 +0530

    Adding DB read class

---
+++ BDDSpecFlowProject/Utility/DBConnection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDDSpecFlowProject.Utility
+{
+   public class DBConnection
+    {
+        public DataTable GetQueryResult(String vConnectionString, String vQuery)
+        {
+            SqlConnection Connection;  // It is for SQL connection
+            DataSet ds = new DataSet();  // it is for store query result
+
+            try
+            {
+                Connection = new SqlConnection(vConnectionString);  // Declare SQL connection with connection string 
+                Connection.Open();  // Connect to Database
+                Console.WriteLine("Connection with database is done.");
+
+                SqlDataAdapter adp = new SqlDataAdapter(vQuery, Connection);  // Execute query on database 
+                adp.Fill(ds);  // Store query result into DataSet object   
+                Connection.Close();  // Close connection 
+                Connection.Dispose();   // Dispose connection             
+            }
+            catch (Exception E)
+            {
+                Console.WriteLine("Error in getting result of query.");
+                Console.WriteLine(E.Message);
+                return new DataTable();
+            }
+            return ds.Tables[0];
+        }
+    }
+}
---
+++ BDDSpecFlowProject/Utility/ExcelData.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using excel = Microsoft.Office.Interop.Excel;
+
+namespace BDDSpecFlowProject.Utility
+{
+   public class ExcelData
+    {
+      
+        public static String getData(int row, int col)
+        {
+            String value="";
+            excel.Application x1app = new excel.Application();
+           // excel.Workbook x1Wb = x1app.Workbooks.Open(@ConfigurationManager.AppSettings["TestDataPath"]);
+            excel.Workbook x1Wb = x1app.Workbooks.Open(@"C:\Users\user\source\repos\BDDSpecFlowProject\BDDSpecFlowProject\Data\ApplicationTestData.xlsx");
+            excel.Worksheet x1Ws = (excel.Worksheet)x1Wb.Sheets[1];
+            excel.Range x1range = x1Ws.UsedRange;
+          //  value = x1range.Cells[row][col].value2;
+           // value = Convert.ToString(x1range.Cells[row][col].value2);
+            x1app.Quit();
+            return value;
+        }
+        [Test]
+        public static void ReadData() {
+           string y= getData(1, 1);
+        }
+    }
+}
